44436a09e4ef35d017952d4b2f7a57ca5bca48b9
xafero <gberendt@outlook.com>
2021-06-03 00:30:56 +0200

    Initial commit

---
+++ digger/Digger.Classic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Digger.Classic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digger!");
+        }
+    }
+}
