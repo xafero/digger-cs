236c6a0ee01b60ba3fa8b2de65a6bf2f789220c2
xafero <gberendt@outlook.com>
2021-06-03 16:40:19 +0200

    Refresher now stores color model too

--- digger/Digger.Classic/Refresher.cs
+++ digger/Digger.Classic/Refresher.cs
@@ -6,10 +6,13 @@ namespace DiggerClassic
     {
         private readonly DrawingArea _area;
 
-        public Refresher(DrawingArea area)
+        public Refresher(DrawingArea area, ColorModel model)
         {
             _area = area;
+            Model = model;
         }
+
+        public ColorModel Model { get; }
 
         public void NewPixels(int x, int y, int w, int h)
             => _area.QueueDrawArea(x, y, w, h);
