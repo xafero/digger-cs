8fb06c0ac7c09137dfb06fd156b3a9c9de15bb67
xafero <gberendt@outlook.com>
2021-06-08 05:38:59 +0200

    Reverting to look like before

--- digger/Digger.Classic/_monster.cs
+++ digger/Digger.Classic/_monster.cs
@@ -1,17 +1,6 @@
 namespace DiggerClassic {
 class _monster {
-  internal int x;
-  internal int y;
-  internal int h,v;
-  internal int xr;
-  internal int yr;
-  internal int dir;
-  internal int hdir,t;
-  internal int hnt;
-  internal int death,bag,dtime;
-  internal int stime;
-  internal bool flag;
-  internal bool nob;
-  internal bool alive;
+  internal int x,y,h,v,xr,yr,dir,hdir,t,hnt,death,bag,dtime,stime;
+  internal bool flag,nob,alive;
 }
 }
