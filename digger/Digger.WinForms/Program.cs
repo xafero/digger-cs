e3f8e026dc53d5bdd2cd2063f901c5914a4d1fd5
xafero <gberendt@outlook.com>
2021-06-25 08:10:52 +0200

    Adding winforms version

---
+++ digger/Digger.WinForms/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DiggerClassic;
+
+namespace Digger.WinForms
+{
+	internal static class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var frm = new FormsDigger(null);
+			var game = new DiggerClassic.Digger(frm);
+			frm.SetFocusable(true);
+			game.Init();
+			game.Start();
+
+			var frame = new Form { Text = "Digger Remastered" };
+			frame.FormClosed += delegate
+			{
+				Application.Exit();
+				Environment.Exit(0);
+			};
+			frame.Size = new Size((int)(game.width * 4.08), (int)(game.height * 4.28));
+			frame.StartPosition = FormStartPosition.CenterScreen;
+
+			var icon = Resources.FindResource("/icons/digger.png");
+			frame.Icon = FormsResources.LoadImage(icon);
+
+			frm._digger = game;
+			frame.Controls.Add(frm);
+			frame.Visible = true;
+
+			Application.Run(frame);
+		}
+	}
+}
---
+++ digger/build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+del *.7z
+
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
+7z a -t7z -mx9 linux-x64.7z ./Digger.GtkSharp/bin/Release/netcoreapp3.1/linux-x64/publish/Digger.GtkSharp
+
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.WinForms
+7z a -t7z -mx9 win-x64.7z   ./Digger.WinForms/bin/Release/netcoreapp3.1/win-x64/publish/Digger.WinForms.exe
+
