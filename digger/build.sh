157c736f391866526e977ef352b3c854102cbeae
xafero <gberendt@outlook.com>
2021-06-25 08:14:54 +0200

    Building release versions with less errors

--- digger/build.sh
+++ digger/build.sh
@@ -2,9 +2,9 @@
 
 rm *.7z
 
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
-7z a -t7z -mx9 linux-x64.7z ./Digger.Classic/bin/Debug/netcoreapp3.1/linux-x64/publish/Digger.Classic
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
+7z a -t7z -mx9 linux-x64.7z ./Digger.GtkSharp/bin/Release/netcoreapp3.1/linux-x64/publish/Digger.GtkSharp
 
-dotnet publish -r win-x64   -p:PublishSingleFile=true --self-contained true
-7z a -t7z -mx9 win-x64.7z   ./Digger.Classic/bin/Debug/netcoreapp3.1/win-x64/publish/Digger.Classic.exe
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
+7z a -t7z -mx9 win-x64.7z   ./Digger.GtkSharp/bin/Release/netcoreapp3.1/win-x64/publish/Digger.GtkSharp.exe
 
