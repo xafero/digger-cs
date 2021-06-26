1df4c6645b5007f9ad28dc05852f044c15ca3771
xafero <xafero@outlook.com>
2021-06-26 04:30:17 +0200

    Renaming second script to match archive name

--- digger/build.sh
+++ digger/build.sh
@@ -6,5 +6,5 @@ dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-containe
 7z a -t7z -mx9 linux-x64.7z ./Digger.GtkSharp/bin/Release/netcoreapp3.1/linux-x64/publish/Digger.GtkSharp
 
 dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
-7z a -t7z -mx9 win-x64.7z   ./Digger.GtkSharp/bin/Release/netcoreapp3.1/win-x64/publish/Digger.GtkSharp.exe
+7z a -t7z -mx9 wing-x64.7z   ./Digger.GtkSharp/bin/Release/netcoreapp3.1/win-x64/publish/Digger.GtkSharp.exe
 
