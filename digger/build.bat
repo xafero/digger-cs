0ed71a7e1cd30c65ba9ab9f215b7ed7d0942bbbe
xafero <xafero@outlook.com>
2021-06-26 04:29:15 +0200

    Added two builds to script

--- digger/build.bat
+++ digger/build.bat
@@ -5,6 +5,12 @@ del *.7z
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
 7z a -t7z -mx9 linux-x64.7z ./Digger.GtkSharp/bin/Release/netcoreapp3.1/linux-x64/publish/Digger.GtkSharp
 
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.GtkSharp
+7z a -t7z -mx9 wing-x64.7z  ./Digger.GtkSharp/bin/Release/netcoreapp3.1/win-x64/publish/Digger.GtkSharp.exe
+
 dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.WinForms
-7z a -t7z -mx9 win-x64.7z   ./Digger.WinForms/bin/Release/netcoreapp3.1/win-x64/publish/Digger.WinForms.exe
+7z a -t7z -mx9 winf-x64.7z  ./Digger.WinForms/bin/Release/netcoreapp3.1/win-x64/publish/Digger.WinForms.exe
+
+dotnet publish -c Release -r win-x64   -p:PublishSingleFile=true --self-contained true Digger.Wpf
+7z a -t7z -mx9 winp-x64.7z  ./Digger.Wpf/bin/Release/netcoreapp3.1/win-x64/publish/Digger.Wpf.exe
 
