7e7ab4cd160da5c96fefa4b876d52b6d2eb975bc
xafero <xafero@outlook.com>
2021-08-15 23:11:34 +0200

    Adding droid as deploy target

---
+++ digger/Digger.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Content.PM;
+using static Android.Content.PM.ConfigChanges;
+using Android.Runtime;
+using Android.OS;
+
+namespace Digger.Droid
+{
+	[Activity(Label = "Digger", Icon = "@mipmap/logo", Theme = "@style/MainTheme", MainLauncher = true, 
+        ConfigurationChanges = ScreenSize | Orientation | UiMode | ScreenLayout | SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static MainActivity()
+        {
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            global::Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ digger/Digger.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using Digger.Xamarin;
+using Xamarin.Forms;
+
+namespace Digger
+{
+	public partial class MainPage : ContentPage
+	{
+		private readonly Stack<int> _keys = new Stack<int>();
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			var moby = GetHandle();
+			if (moby._digger != null)
+				return;
+			var game = new DiggerClassic.Digger(moby);
+			moby._digger = game;
+			moby.SetFocusable();
+			game.Init();
+			game.Start();
+		}
+
+		private MobileDigger GetHandle() => (MobileDigger)FindByName("myCanvas");
+
+		private void DownButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_Down);
+		private void UpButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_Up);
+		private void RightButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_Right);
+		private void LeftButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_Left);
+		private void FireButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_F1);
+		private void StopButton_Clicked(object sender, EventArgs e) => SendKey(AppletCompat.Key_F10);
+		private void InputButton_Clicked(object sender, EventArgs e) => SendKey('a');
+
+		private void SendKey(int key)
+		{
+			var dig = GetHandle();
+			while (_keys.Count >= 1)
+				dig.KeyUp(_keys.Pop());
+			dig.KeyDown(key);
+			_keys.Push(key);
+		}
+
+		private double lastWidth;
+		private double lastHeight;
+
+		private StackOrientation _orientation;
+		public StackOrientation Orientation
+		{
+			get => _orientation;
+			set
+			{
+				_orientation = value;
+				OnPropertyChanged(nameof(Orientation));
+				UpdateOrientation(_orientation);
+			}
+		}
+
+		protected override void OnSizeAllocated(double width, double height)
+		{
+			base.OnSizeAllocated(width, height);
+			if (width == lastWidth && height == lastHeight)
+				return;
+			lastWidth = width;
+			lastHeight = height;
+			StackOrientation orientation;
+			if (width > height)
+				orientation = StackOrientation.Horizontal;
+			else
+				orientation = StackOrientation.Vertical;
+			Orientation = orientation;
+		}
+
+		private void UpdateOrientation(StackOrientation orientation)
+		{
+			switch (orientation)
+			{
+				case StackOrientation.Horizontal:
+					leftButtons.IsVisible = true;
+					rightButtons.IsVisible = true;
+					allButtons.IsVisible = false;
+					Grid.SetRow(diggerFrame, 0);
+					Grid.SetColumn(diggerFrame, 1);
+					Grid.SetRowSpan(diggerFrame, 2);
+					Grid.SetColumnSpan(diggerFrame, 1);
+					Grid.SetRow(leftButtons, 0);
+					Grid.SetColumn(leftButtons, 0);
+					Grid.SetRowSpan(leftButtons, 2);
+					Grid.SetColumnSpan(leftButtons, 1);
+					Grid.SetRow(rightButtons, 0);
+					Grid.SetColumn(rightButtons, 2);
+					Grid.SetRowSpan(rightButtons, 2);
+					Grid.SetColumnSpan(rightButtons, 1);
+					break;
+				case StackOrientation.Vertical:
+					leftButtons.IsVisible = false;
+					rightButtons.IsVisible = false;
+					allButtons.IsVisible = true;
+					Grid.SetRow(diggerFrame, 0);
+					Grid.SetColumn(diggerFrame, 0);
+					Grid.SetRowSpan(diggerFrame, 1);
+					Grid.SetColumnSpan(diggerFrame, 3);
+					Grid.SetRow(allButtons, 1);
+					Grid.SetColumn(allButtons, 0);
+					Grid.SetRowSpan(allButtons, 1);
+					Grid.SetColumnSpan(allButtons, 3);
+					break;
+			}
+		}
+	}
+}
