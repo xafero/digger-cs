00023fd292f678e849db3f552aff5a9a44276848
xafero <xafero@google.com>
2025-02-22 15:59:41 +0100

    Removing stuff

--- digger/Digger.WinUI/App.xaml.cs
+++ digger/Digger.WinUI/App.xaml.cs
@@ -1,19 +1,8 @@
 ﻿
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Digger.Maui.WinUI
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : MauiWinUIApplication
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
@@ -21,5 +10,4 @@ namespace Digger.Maui.WinUI
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
-
 }
