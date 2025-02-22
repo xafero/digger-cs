916d9d289d312a10e3cfbff8ec85cf442a85717a
xafero <xafero@google.com>
2025-02-22 14:28:35 +0100

    Adding basic projects

---
+++ digger/Digger.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Digger.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ digger/Digger.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Digger.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
