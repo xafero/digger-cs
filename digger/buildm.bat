0d37e18329e83c6ce4a34cae0dca1eb74d9a5b64
xafero <xafero@google.com>
2026-04-15 22:24:45 +0200

    Updating

--- digger/buildm.bat
+++ digger/buildm.bat
@@ -1,6 +1,6 @@
 @echo off
 
-dotnet build -f net8.0-windows10.0.19041.0 -r win10-x86 -c Debug Digger.WinUI
+dotnet build -f net10.0-windows10.0.19041.0 -r win10-x86 -c Debug Digger.WinUI
 
-dotnet run -f net8.0-windows10.0.19041.0 -r win10-x86 -c Debug --project Digger.WinUI
+dotnet run -f net10.0-windows10.0.19041.0 -r win10-x86 -c Debug --project Digger.WinUI
 
