d42f7018fc55a3e2a96e5a57d7b09048370e1410
xafero <xafero@google.com>
2026-04-17 01:02:57 +0200

    Fixing

--- digger/a/Digger.Avalonia/Views/MainView.axaml.cs
+++ digger/a/Digger.Avalonia/Views/MainView.axaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Avalonia;
 using Digger.Xamarin;
 using Avalonia.Controls;
+using Avalonia.Layout;
 
 // ReSharper disable InconsistentNaming
 
@@ -54,6 +55,75 @@ namespace Digger.Views
                 dig.KeyUp(_keys.Pop());
             dig.KeyDown(key);
             _keys.Push(key);
+        }
+
+        private double lastWidth;
+        private double lastHeight;
+
+        private Orientation _myOrientation;
+
+        public Orientation MyOrientation
+        {
+            get => _myOrientation;
+            set { _myOrientation = value; UpdateOrientation(_myOrientation); }
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var size = base.ArrangeOverride(finalSize);
+            OnSizeAllocated(size.Width, size.Height);
+            return size;
+        }
+
+        protected void OnSizeAllocated(double width, double height)
+        {
+            if ((int)width == (int)lastWidth && (int)height == (int)lastHeight)
+                return;
+            lastWidth = width;
+            lastHeight = height;
+            Orientation orientation;
+            if (width > height)
+                orientation = Orientation.Horizontal;
+            else
+                orientation = Orientation.Vertical;
+            MyOrientation = orientation;
+        }
+
+        private void UpdateOrientation(Orientation orientation)
+        {
+            switch (orientation)
+            {
+                case Orientation.Horizontal:
+                    leftButtons.IsVisible = true;
+                    rightButtons.IsVisible = true;
+                    allButtons.IsVisible = false;
+                    Grid.SetRow(diggerFrame, 0);
+                    Grid.SetColumn(diggerFrame, 1);
+                    Grid.SetRowSpan(diggerFrame, 2);
+                    Grid.SetColumnSpan(diggerFrame, 1);
+                    Grid.SetRow(leftButtons, 0);
+                    Grid.SetColumn(leftButtons, 0);
+                    Grid.SetRowSpan(leftButtons, 2);
+                    Grid.SetColumnSpan(leftButtons, 1);
+                    Grid.SetRow(rightButtons, 0);
+                    Grid.SetColumn(rightButtons, 2);
+                    Grid.SetRowSpan(rightButtons, 2);
+                    Grid.SetColumnSpan(rightButtons, 1);
+                    break;
+                case Orientation.Vertical:
+                    leftButtons.IsVisible = false;
+                    rightButtons.IsVisible = false;
+                    allButtons.IsVisible = true;
+                    Grid.SetRow(diggerFrame, 0);
+                    Grid.SetColumn(diggerFrame, 0);
+                    Grid.SetRowSpan(diggerFrame, 1);
+                    Grid.SetColumnSpan(diggerFrame, 3);
+                    Grid.SetRow(allButtons, 1);
+                    Grid.SetColumn(allButtons, 0);
+                    Grid.SetRowSpan(allButtons, 1);
+                    Grid.SetColumnSpan(allButtons, 3);
+                    break;
+            }
         }
     }
 }
